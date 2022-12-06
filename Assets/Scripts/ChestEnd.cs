1dec1851429c95ca6e7d33c934b570650439badd
Cole Beezley <cdbbeezley@gmail.com>
2022-12-05 18:07:33 -0800

    Finalized levels, improved SceneManager

--- Assets/Scripts/ChestEnd.cs
+++ Assets/Scripts/ChestEnd.cs
@@ -8,18 +8,20 @@ public class ChestEnd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnCollisionEnter2D(Collision2D other){
-        if (other.gameObject.CompareTag("Player")){
-            SceneManager.LoadScene("Level 2");
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
