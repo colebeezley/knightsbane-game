b97abf560d352356ca0655b26e67f30653d70c9d
Cole Beezley <cdbbeezley@gmail.com>
2022-12-09 00:10:31 -0800

    Added basic enemy

---
+++ Assets/Scripts/arrowFire.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowFire : MonoBehaviour
+{
+
+    public GameObject trigger;
+    public GameObject arrow;
+
+    private bool moveArrow = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveArrow)
+        {
+            arrow.transform.Translate(7 * Time.deltaTime, 0, 0);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            moveArrow = true;
+        }
+        
+        // StartCoroutine(FireArrow());
+    }
+
+    IEnumerator FireArrow()
+    {
+
+        arrow.transform.Translate(1, 0, 0);
+        yield return null;
+    }
+}
