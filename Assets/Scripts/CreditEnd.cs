7274a6536c488015127ead76b7a3d0f7dba82fd2
Cole Beezley <cdbbeezley@gmail.com>
2022-12-11 17:28:42 -0800

    Added New Game+

--- Assets/Scripts/CreditEnd.cs
+++ Assets/Scripts/CreditEnd.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class CreditEnd : MonoBehaviour
 {
+
+    public bool newgame;
     // Start is called before the first frame update
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(0);
+            if (newgame)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
         }
-
     }
 }
